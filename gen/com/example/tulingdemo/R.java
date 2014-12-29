3193084ae949f9d9e4a6f16e1262843c1be4c0cf
zhouyu0615 <871211719@qq.com>
2014-12-29 17:29:51 +0800

    添加设置，退出，软件说明的菜单

--- gen/com/example/tulingdemo/R.java
+++ gen/com/example/tulingdemo/R.java
@@ -38,18 +38,25 @@ public final class R {
         public static final int zhouyu=0x7f02000a;
     }
     public static final class id {
-        public static final int TimeText=0x7f090003;
-        public static final int action_settings=0x7f090006;
-        public static final int iv=0x7f090004;
-        public static final int listView1=0x7f090000;
-        public static final int sendText=0x7f090001;
-        public static final int send_btn=0x7f090002;
-        public static final int tv=0x7f090005;
+        public static final int TimeText=0x7f090006;
+        public static final int about=0x7f09000b;
+        public static final int action_settings=0x7f090009;
+        public static final int iv=0x7f090007;
+        public static final int listView1=0x7f090003;
+        public static final int quit=0x7f09000a;
+        public static final int sendText=0x7f090004;
+        public static final int send_btn=0x7f090005;
+        public static final int textView1=0x7f090000;
+        public static final int tv=0x7f090008;
+        public static final int voiceSwitcher=0x7f090001;
+        public static final int voicechooseSwitcher=0x7f090002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int leftitem=0x7f030001;
-        public static final int rightitem=0x7f030002;
+        public static final int about=0x7f030000;
+        public static final int action_setting=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int leftitem=0x7f030003;
+        public static final int rightitem=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -57,10 +64,17 @@ public final class R {
     public static final class string {
         public static final int NetConnectFailed=0x7f050004;
         public static final int Servefailed=0x7f050005;
+        public static final int about=0x7f050007;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int man_voice=0x7f050009;
+        public static final int quit=0x7f050006;
         public static final int send=0x7f050003;
+        public static final int voice=0x7f050008;
+        public static final int voice_off=0x7f05000c;
+        public static final int voice_on=0x7f05000b;
+        public static final int woman_voice=0x7f05000a;
     }
     public static final class style {
         /** 
--- src/com/example/tulingdemo/MainActivity.java
+++ src/com/example/tulingdemo/MainActivity.java
@@ -21,6 +21,8 @@ import android.R.string;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -39,6 +41,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baidu.speechsynthesizer.SpeechSynthesizer;
 import com.baidu.speechsynthesizer.SpeechSynthesizerListener;
@@ -49,17 +52,17 @@ import com.baidu.speechsynthesizer.publicutility.SpeechError;
 public class MainActivity extends Activity implements HttpGetDataListener,
 		android.view.View.OnClickListener {
 
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
-	     super.onCreateOptionsMenu(menu);
-	     
-	     MenuInflater mInflater=getMenuInflater();
-	     mInflater.inflate(R.menu.main, menu);
-	     
-	     return true;
+		super.onCreateOptionsMenu(menu);
+
+		MenuInflater mInflater = getMenuInflater();
+		mInflater.inflate(R.menu.main, menu);
+
+		return true;
 	}
+
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		// TODO Auto-generated method stub
@@ -68,15 +71,33 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
-		return super.onOptionsItemSelected(item);
+		int item_id = item.getItemId();
+
+		System.out.println(item);
+
+		switch (item_id) {
+		case R.id.action_settings:
+			Intent intent = new Intent();
+			intent.setClass(MainActivity.this, action_settings.class);
+			startActivity(intent);
+
+			break;
+		case R.id.quit:
+			MainActivity.this.finish();
+			break;
+		case R.id.about:
+			Intent i = new Intent();
+			i.setClass(MainActivity.this, about.class);
+			startActivity(i);
+			break;
+
+		default:
+			break;
+		}
+
+		// return super.onOptionsItemSelected(item);
+		return true;
 	}
-
-
-
-
-
-	private static final OnClickListener OnClickListener = null;
 
 	private String infoString = "http://www.tuling123.com/openapi/api?"
 			+ "key=f7f0654e45e5fb1638e7e89d5d310c3c&info=你好";
@@ -88,18 +109,52 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 	private ListView listView;
 
 	private TextAdapter textAdapter;
-	
+
 	private long currentTime, oldTime;
-	
-	private static SpeechSynthesizerThread speechThread;
+
+	public SettingParams sParams;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		initView();       	
-		setVolumeControlStream(AudioManager.STREAM_MUSIC);		 
+		GetSettingFormFile();
+
+		initView();
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+	}
+
+	protected void GetSettingFormFile() {
+		SharedPreferences settingPreferences = getSharedPreferences(
+				action_settings.SETTINGS, Activity.MODE_PRIVATE);
+
+		SharedPreferences.Editor editor = settingPreferences.edit();
+		sParams = new SettingParams();
+		
+		if (settingPreferences.getBoolean("first", true)) {
+			// Toast.makeText(getApplicationContext(), "第一次使用，不存在Settingfile",
+			// Toast.LENGTH_LONG).show();
+			editor.putBoolean("first", false);
+			editor.putBoolean(action_settings.VOICE_ON, true); // 默认开启语音朗读
+			editor.putBoolean(action_settings.VOICE_MAN, false);// 默认女声
+			editor.commit();
+			sParams.setVoice_on_flag(true);
+			sParams.setMan_voice(false);
+
+		} else {
+			// Toast.makeText(getApplicationContext(),
+			// " 不是第一次使用，存在setting file",
+			// Toast.LENGTH_LONG).show();
+
+			sParams.setMan_voice(settingPreferences.getBoolean(
+					action_settings.VOICE_MAN, false));
+			sParams.setVoice_on_flag(settingPreferences.getBoolean(
+					action_settings.VOICE_ON, true));
+
+		}
 	}
 
 	public void initView() {
@@ -114,18 +169,20 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 
 		listView.setAdapter(textAdapter);
 
-		String welString=GetWelcomeSentence();	
-		
-		new SpeechSynthesizerThread(getApplicationContext(), welString).start();
-		
-		ListData listData = new ListData(welString,
-				ListData.RECEIVER,getTime());
+		String welString = GetWelcomeSentence();
+
+		SpeechVoice(welString,sParams);
+
+		ListData listData = new ListData(welString, ListData.RECEIVER,
+				getTime());
 		lists.add(listData);
-		
-		if (isNetConnected(this)==false) {
-			String netFailedstring=getResources().getString(R.string.NetConnectFailed);
-			lists.add(new ListData(netFailedstring, ListData.RECEIVER,getTime()));
-			
+
+		if (isNetConnected(this) == false) {
+			String netFailedstring = getResources().getString(
+					R.string.NetConnectFailed);
+			lists.add(new ListData(netFailedstring, ListData.RECEIVER,
+					getTime()));
+
 		}
 
 		// HttpData httpData = (HttpData) new HttpData(infoString,
@@ -145,54 +202,43 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 		return welcome;
 	}
 
+	
+	protected void SpeechVoice(String speechString,SettingParams sParams) {
+		if(sParams.isVoice_on_flag())
+		{
+		  new SpeechSynthesizerThread(getApplicationContext(), speechString,sParams).start();
+		}
+		
+	}
+	
+	
 	@Override
 	public void GetDataUrl(final String data) {
 		// TODO Auto-generated method stub
-		 System.out.println(data);
-		 
-//		 new Thread(new Runnable() {
-//			
-//			@Override
-//			public void run() {
-//				// TODO Auto-generated method stub
-//				try {
-//					setParams();
-//					int ret=speechSynthesizer.speak(getStringFormJson(data));
-//					System.out.println("------->ret="+ret);
-//				} catch (JSONException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//				
-//			}
-//		}).start();
-		 
-		 ParseText(data);
+		System.out.println(data);
+
+		ParseText(data);
 	}
 
 	public void ParseText(String str) {
-	
-		try {	
-			if(str!="")
-			{
-			String getString=getStringFormJson(str);
-				
-			new SpeechSynthesizerThread(getApplicationContext(), getString).start();	
-			ListData  lData = new ListData(getString,
-					ListData.RECEIVER,getTime());
-			
-			lists.add(lData);
-			}else {
-			ListData	lData=new ListData(getResources().getString(R.string.Servefailed), 
-						ListData.RECEIVER, getTime());
-			lists.add(lData);
-			
 
-			
+		try {
+			if (str != "") {
+				String getString = getStringFormJson(str);
 
-			
+				SpeechVoice(getString, sParams);
+
+				ListData lData = new ListData(getString, ListData.RECEIVER,
+						getTime());
+
+				lists.add(lData);
+			} else {
+				ListData lData = new ListData(getResources().getString(
+						R.string.Servefailed), ListData.RECEIVER, getTime());
+				lists.add(lData);
+
 			}
-			//speechThread.setSpeechString(getStringFormJson(str)); 
+			// speechThread.setSpeechString(getStringFormJson(str));
 			textAdapter.notifyDataSetChanged();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -200,66 +246,58 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 		}
 
 	}
-	
 
+	private static final int TEXT_INFO = 100000; // 文本类数据
+	private static final int URL_INFO = 200000; // 网址类数据
+	private static final int NOVEL_INFO = 301000; // 小说
+	private static final int NEWS_INFO = 302000; // 新闻
+	private static final int DOWNLOAD_INFO = 304000; // 应用、软件、下载
+	private static final int TRAIN_INFO = 305000; // 列车
+	private static final int FLIGHT_INFO = 306000; // 航班
+	private static final int GROUPBUY_INFO = 307000; // 团购
+	private static final int DISCOUNT_INFO = 308000; // 优惠
+	private static final int HOTEL_INFO = 309000; // 酒店
+	private static final int LOTTERY_INFO = 310000; // 彩票
+	private static final int PRICE_INFO = 311000; // 价格
+	private static final int RESTAURANT_INFO = 312000; // 餐厅
+	private static final int LENGTHERROR_INFO = 40001; // key的长度错误（32位）
+	private static final int CONTENTNULL_INFO = 40002; // 请求内容为空
+	private static final int KEYERROR_INFO = 40003; // key错误或帐号未激活
+	private static final int REQUESTTIMESOUT_INFO = 40004; // 当天请求次数已用完
+	private static final int NOTSUPPORT_INFO = 40005; // 暂不支持该功能
+	private static final int SERVERICEUPGRADE_INFO = 40006; // 服务器升级中
+	private static final int DATAEXCEPTION_INFO = 40007; // 服务器数据格式异常
+	private static final int DEFAULT_INFO = 50000; // 机器人设定的“学用户说话”或者“默认回答”
 
-	private static final int TEXT_INFO=100000;	 //文本类数据
-	private static final int URL_INFO=200000;	// 网址类数据
-	private static final int NOVEL_INFO=301000;	// 小说
-	private static final int NEWS_INFO=302000;	// 新闻
-	private static final int DOWNLOAD_INFO=304000;	// 应用、软件、下载
-	private static final int TRAIN_INFO=305000;	// 列车
-	private static final int FLIGHT_INFO=306000;	// 航班
-	private static final int GROUPBUY_INFO=	307000;	// 团购
-	private static final int DISCOUNT_INFO=	308000;	// 优惠
-	private static final int HOTEL_INFO=	309000;	 //酒店
-	private static final int LOTTERY_INFO=310000;	// 彩票
-	private static final int PRICE_INFO=311000;	// 价格
-	private static final int RESTAURANT_INFO=312000;	// 餐厅
-	private static final int LENGTHERROR_INFO=40001;	// key的长度错误（32位）
-	private static final int CONTENTNULL_INFO=40002	; //请求内容为空
-	private static final int KEYERROR_INFO=40003;	// key错误或帐号未激活
-	private static final int REQUESTTIMESOUT_INFO=40004;	 //当天请求次数已用完
-	private static final int NOTSUPPORT_INFO=40005;	 //暂不支持该功能
-	private static final int SERVERICEUPGRADE_INFO=40006;	// 服务器升级中
-	private static final int DATAEXCEPTION_INFO=40007;	 //服务器数据格式异常
-	private static final int DEFAULT_INFO=50000;	 //机器人设定的“学用户说话”或者“默认回答”
-	
-	
 	private String getStringFormJson(String str) throws JSONException {
 		StringBuffer resultString = null;
 		JSONObject jsonObject;
-		
+
 		jsonObject = new JSONObject(str);
 		System.out.println(jsonObject.getString("code"));
-		
-		resultString=new StringBuffer(jsonObject.getString("text"));
+
+		resultString = new StringBuffer(jsonObject.getString("text"));
 		resultString.append(" ");
-        
-		int code=Integer.parseInt(jsonObject.getString("code"));
-		
-     
-		
+
+		int code = Integer.parseInt(jsonObject.getString("code"));
+
 		switch (code) {
-		case URL_INFO :
+		case URL_INFO:
 			resultString.append(jsonObject.getString("url"));
-			System.out.println("------->"+jsonObject.getString("url"));
+			System.out.println("------->" + jsonObject.getString("url"));
 			break;
-		case NEWS_INFO :
+		case NEWS_INFO:
 			resultString.append(jsonObject.getString("url"));
-			resultString.append(jsonObject.getString("list"));			
+			resultString.append(jsonObject.getString("list"));
 			break;
-	
 
 		default:
 			break;
 		}
-		
-				
-		System.out.println("resultString"+resultString);
-		return resultString.toString();		
+
+		System.out.println("resultString" + resultString);
+		return resultString.toString();
 	}
-	
 
 	@Override
 	public void onClick(View v) {
@@ -269,14 +307,15 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 		String sendString = contentString.replace(" ", "");
 		sendString = sendString.replace("\n", "");
 
-		ListData sendData = new ListData(contentString, ListData.SEND,getTime());
+		ListData sendData = new ListData(contentString, ListData.SEND,
+				getTime());
 		lists.add(sendData);
-		
-		if (lists.size()>=100) {
-			for (int i = 0; i < lists.size()-30; i++) {
+
+		if (lists.size() >= 100) {
+			for (int i = 0; i < lists.size() - 30; i++) {
 				lists.remove(i);
 			}
-			
+
 		}
 		textAdapter.notifyDataSetChanged();
 		HttpData httpData = (HttpData) new HttpData(
@@ -299,33 +338,44 @@ public class MainActivity extends Activity implements HttpGetDataListener,
 		}
 		return false;
 	}
-	
-	@SuppressLint("SimpleDateFormat") public String getTime() {
-		String timeString=null;
-		
-		currentTime=System.currentTimeMillis();
-		
-		if (currentTime-oldTime>=3*50*1000) {	
-		    oldTime=currentTime;
-		SimpleDateFormat format= new SimpleDateFormat("yyyy年MM月dd日  HH:mm:ss");		
-		Date curdate=new Date(currentTime);
-		timeString=format.format(curdate);
-		
-		System.out.println("------->"+timeString);
-			
-		return timeString;
-		}
-		else 
-		{
+
+	@SuppressLint("SimpleDateFormat")
+	public String getTime() {
+		String timeString = null;
+
+		currentTime = System.currentTimeMillis();
+
+		if (currentTime - oldTime >= 3 * 50 * 1000) {
+			oldTime = currentTime;
+			SimpleDateFormat format = new SimpleDateFormat(
+					"yyyy年MM月dd日  HH:mm:ss");
+			Date curdate = new Date(currentTime);
+			timeString = format.format(curdate);
+
+			System.out.println("------->" + timeString);
+
+			return timeString;
+		} else {
 			return "";
-			
+
 		}
-		
+
 	}
 
 	
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		GetSettingFormFile();
+	}
 	
-	  
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		System.out.println("----->main activity onDestory!");
+		super.onDestroy();
+	}
 
-	
 }
